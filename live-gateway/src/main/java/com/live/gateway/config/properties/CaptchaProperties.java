56a22390e23dee9dc4a136182a77f0328941b9d9
ganzhe <ganzhe@gmail.com>
2022-11-01 20:48:16 +0800

    live-system

---
+++ live-gateway/src/main/java/com/live/gateway/config/properties/CaptchaProperties.java
@@ -0,0 +1,32 @@
+package com.live.gateway.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 验证码配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+@RefreshScope
+@ConfigurationProperties(prefix = "security.captcha")
+@Getter
+@Setter
+public class CaptchaProperties
+{
+    /**
+     * 验证码开关
+     */
+    private Boolean enabled;
+
+    /**
+     * 验证码类型（math 数组计算 char 字符）
+     */
+    private String type;
+
+
+}
