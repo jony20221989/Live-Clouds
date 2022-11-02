e2c0ee598603cd0b9f58572e52e0e087c1d60b54
ganzhe <ganzhe@gmail.com>
2022-11-02 17:12:28 +0800

    授权

---
+++ live-auth/src/main/java/com/live/auth/model/LoginBody.java
@@ -0,0 +1,26 @@
+package com.live.auth.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 用户登录对象
+ * 
+ * @author ruoyi
+ */
+@Getter
+@Setter
+public class LoginBody
+{
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+
+}
---
+++ live-modules/live-modules-system/src/main/java/com/live/system/controller/SysOperlogController.java
@@ -0,0 +1,73 @@
+package com.live.system.controller;
+
+import com.live.common.core.utils.poi.ExcelUtil;
+import com.live.common.core.web.controller.BaseController;
+import com.live.common.core.web.domain.AjaxResult;
+import com.live.common.core.web.page.TableDataInfo;
+import com.live.common.log.annotation.Log;
+import com.live.common.log.enums.BusinessType;
+import com.live.common.security.annotation.InnerAuth;
+import com.live.common.security.annotation.RequiresPermissions;
+import com.live.system.api.domain.SysOperLog;
+import com.live.system.service.ISysOperLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 操作日志记录
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/operlog")
+public class SysOperlogController extends BaseController
+{
+    @Autowired
+    private ISysOperLogService operLogService;
+
+    @RequiresPermissions("system:operlog:list")
+    @GetMapping("/list")
+    public TableDataInfo list(SysOperLog operLog)
+    {
+        startPage();
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        return getDataTable(list);
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.EXPORT)
+    @RequiresPermissions("system:operlog:export")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysOperLog operLog)
+    {
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        ExcelUtil<SysOperLog> util = new ExcelUtil<SysOperLog>(SysOperLog.class);
+        util.exportExcel(response, list, "操作日志");
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.DELETE)
+    @RequiresPermissions("system:operlog:remove")
+    @DeleteMapping("/{operIds}")
+    public AjaxResult remove(@PathVariable Long[] operIds)
+    {
+        return toAjax(operLogService.deleteOperLogByIds(operIds));
+    }
+
+    @RequiresPermissions("system:operlog:remove")
+    @Log(title = "操作日志", businessType = BusinessType.CLEAN)
+    @DeleteMapping("/clean")
+    public AjaxResult clean()
+    {
+        operLogService.cleanOperLog();
+        return AjaxResult.success();
+    }
+
+    @InnerAuth
+    @PostMapping
+    public AjaxResult add(@RequestBody SysOperLog operLog)
+    {
+        return toAjax(operLogService.insertOperlog(operLog));
+    }
+}
