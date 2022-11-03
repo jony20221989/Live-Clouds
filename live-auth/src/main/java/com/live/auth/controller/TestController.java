56a50583f144e1f55ce98df31f6e2cebf4f14c29
ganzhe <ganzhe@gmail.com>
2022-11-03 20:22:06 +0800

    swagger

---
+++ live-auth/src/main/java/com/live/auth/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.live.auth.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/tGet")
+    public String tGet(){
+        return "tGet";
+    }
+}
--- live-common/live-common-swagger/src/main/java/com/live/common/swagger/annotation/EnableCustomSwagger2.java
+++ live-common/live-common-swagger/src/main/java/com/live/common/swagger/annotation/EnableCustomSwagger2.java
@@ -1,16 +1,17 @@
-package com.live.common.swagger.annotation;
-
-import com.live.common.swagger.config.SwaggerAutoConfiguration;
-import org.springframework.context.annotation.Import;
-
-import java.lang.annotation.*;
-
-@Target({ ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-@Inherited
-@Import({ SwaggerAutoConfiguration.class })
-public @interface EnableCustomSwagger2
-{
-
-}
+//package com.live.common.swagger.annotation;
+//
+////import com.live.common.swagger.config.SwaggerAutoConfiguration;
+//import com.live.common.swagger.config.SwaggerConfiguration;
+//import org.springframework.context.annotation.Import;
+//
+//import java.lang.annotation.*;
+//
+//@Target({ ElementType.TYPE })
+//@Retention(RetentionPolicy.RUNTIME)
+//@Documented
+//@Inherited
+//@Import({ SwaggerAutoConfiguration.class })
+//public @interface EnableCustomSwagger2
+//{
+//
+//}
